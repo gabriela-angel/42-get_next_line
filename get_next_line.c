bcb08c4f8ed6ecd80d4adf771f31bc19d00e7472
gangel-a <gangel-a@student.42sp.org.br>
2024-10-23 15:00:30 -0300

    starts working on the logic to solve the problem and adds utils

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gangel-a <gangel-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:40:00 by gangel-a          #+#    #+#             */
-/*   Updated: 2024/10/22 20:17:28 by gangel-a         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:15:19 by gangel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,20 @@
 
 char	*get_next_line(int fd)
 {
+	// NAO TRATADO:
+//E SE A GNT AINDA NAO TIVER CHEGADO NUMA queBRA De LINHA E O ARQUIVO NSO TIVER TERMINADO)
 	char		*line;
 	static char	*rest;
+	size_t		rest_len;
 	size_t		len;
 	size_t		i;
 	char		*buffer;
 	size_t		bytes_read;
+	char		*temp;
 
+	i = 0;
+
+	// PROCESSO DE LEITURA
 	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
 	if (!buffer)
 		return (NULL);
@@ -29,31 +36,75 @@ char	*get_next_line(int fd)
 		return (NULL);
 	buffer[bytes_read] = '\0';
 
-	rest = NULL;
-	len = line_len(buffer);
-	if (len < bytes_read)
-	{
-		rest = (char *)malloc(bytes_read - len + 1);
-		if (!rest)
-			return (NULL);
-		i = len;
-		while (i <= bytes_read)
-			rest[i++] = buffer[i++];
-	}
+	// ONLY IF REST == NULL;
+	// CREATE REST
+	rest_len = bytes_read - line_len(buffer);
+	rest = (char *)malloc(rest_len + 1);
+	if (!rest)
+		return (NULL);
 
-	i = 0;
+	// CREATE LINE
+	if (*rest)
+	{
+		len = line_len(rest);
+		if (rest[len] != '\n')
+			len += line_len(buffer);
+	}
+	else
+		len = line_len(buffer);
 	line = (char *)malloc(len + 1);
 	if (!line)
 		return (NULL);
-	while (i <= len)
-		line[i++] = buffer[i++];
-	line[i] = '\0';
 	return (line);
+
+	// POPULATE LINE
+	i = 0;
+	j = 0;
+	if (!*rest)
+	{
+		while (rest[i] != '\n' && rest[i])
+			line[i++] = rest[i++];
+		if (rest[i] == '\n')
+		{
+			line[i] == rest[i];
+			return (line);
+		}
+	}
+	while (buffer[j] != '\n' && buffer[j])
+		line[i] = buffer[j];
+	if (buffer[j] == '\n')
+		line[i] == buffer[j];
+	return (line);
+
+	// IF *REST = NULL
+	if (*rest == NULL)
+		// POPULATE REST
+		while (buffer[len + i])
+			rest[i++] = buffer[len + i++];
+	else
+	// ELSE IF *REST != NULL
+		//REMOVE PART THAT WAS COPIED TO LINE FROM REST
+		//AND FREE IF REST GETS EMPTY
+		while ()
+		{
+
+		}
 
 }
 
-char	*read_line(int fd)
+int	line_count(char *str)
 {
+	int	lines;
+	int	i;
+
+	lines = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			lines++;
+		i++;
+	}
+	return (lines);
 }
 
 size_t	line_len(char *str)
@@ -61,7 +112,7 @@ size_t	line_len(char *str)
 	size_t	len;
 
 	len = 0;
-	while (str)
+	while (str[len])
 	{
 		if (str[len] == '\n')
 			return (len + 1);
@@ -70,9 +121,12 @@ size_t	line_len(char *str)
 	return (len);
 }
 
-void	call_gnl(void)
+#include <fcntl.h>
+
+void	main(void)
 {
+	int	fd = open("teste.txt", O_RDWR);
+	
 	while (get_next_line != NULL)
 		get_next_line(fd);
-
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,56 @@
 /*   By: gangel-a <gangel-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:42:12 by gangel-a          #+#    #+#             */
-/*   Updated: 2024/10/22 15:42:26 by gangel-a         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:58:07 by gangel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (*ptr)
+	{
+		if (*ptr == (unsigned char)c)
+			return (ptr);
+		ptr++;
+	}
+	if ((unsigned char)c == '\0')
+		return (ptr);
+	return (NULL);
+}
+
+//char	*ft_strjoin(char const *s1, char const *s2)
+//{
+
+//}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	len_s;
+	size_t	i;
+
+	i = 0;
+	len_s = ft_strlen(src);
+	if (size == 0)
+		return (len_s);
+	while (i < size - 1 && src[i])
+	{
+		dst[i] == src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (len_s);
+}
+
+//char	*ft_substr(char const *s, unsigned int start, size_t len)
+//{
+//	char	*subs;
+//	size_t	s_len;
+
+//	if (!s)
+//		return ()
+//}
